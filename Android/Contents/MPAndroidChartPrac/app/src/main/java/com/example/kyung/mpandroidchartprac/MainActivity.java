dab5e76d9b04b6c817dc2c13888851e90f5cda11
KyungSeok <leeseok8347@gmail.com>
2017-12-13 16:09:13 +0900

    edit chart

--- Android/Contents/MPAndroidChartPrac/app/src/main/java/com/example/kyung/mpandroidchartprac/MainActivity.java
+++ Android/Contents/MPAndroidChartPrac/app/src/main/java/com/example/kyung/mpandroidchartprac/MainActivity.java
@@ -1,13 +1,16 @@
 package com.example.kyung.mpandroidchartprac;
 
+import android.content.Context;
 import android.graphics.Color;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.webkit.WebIconDatabase;
 
+import com.github.mikephil.charting.animation.Easing;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.components.AxisBase;
+import com.github.mikephil.charting.components.Description;
 import com.github.mikephil.charting.components.XAxis;
 import com.github.mikephil.charting.components.YAxis;
 import com.github.mikephil.charting.data.Entry;
@@ -15,6 +18,7 @@ import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
 import com.github.mikephil.charting.formatter.IAxisValueFormatter;
 import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.mikephil.charting.utils.ColorTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
 
     List<Entry> entries1 = new ArrayList<>();
     List<Entry> entries2 = new ArrayList<>();
+    List<Integer> colors = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -46,14 +51,20 @@ public class MainActivity extends AppCompatActivity {
         // 1. 차트에 들어갈 데이터 정의
         for(int i=1 ; i<11 ; i++){
             entries2.add(new Entry(i,i*3));
-            if(i%2==0) entries1.add(new Entry(i, i*4));
-            else entries1.add(new Entry(i, i));
+            if(i%2==0) {
+                entries1.add(new Entry(i, i * 4));
+                colors.add(Color.DKGRAY);
+            }
+            else {
+                entries1.add(new Entry(i, i));
+                colors.add(Color.MAGENTA);
+            }
         }
 
         // 2. LineDataSet을 정의 (data, 범례이름) + 정의한 값의 속성 정의
         LineDataSet dataSet = new LineDataSet(entries1,"LINE TEST 1");
         dataSet.setAxisDependency(YAxis.AxisDependency.LEFT);
-        dataSet.setColor(Color.MAGENTA);
+        dataSet.setColors(colors);
         dataSet.setValueTextColor(Color.BLUE);
 
         LineDataSet dataSet2 = new LineDataSet(entries2,"LINE TEST 2");
@@ -66,12 +77,18 @@ public class MainActivity extends AppCompatActivity {
             // x축에 있는 value들을 가져오는 콜백함수
             @Override
             public String getFormattedValue(float value, AxisBase axis) {
-                Log.e("value","======================"+value);
                 String x = "Q"+ (int)value;
                 return x;
             }
         };
         XAxis xAxis = lineChart.getXAxis(); // x축 스타일링시작
+        xAxis.setPosition(XAxis.XAxisPosition.BOTTOM); // x축 위치 지정
+        xAxis.setTextSize(10f); // 크기 지정
+        xAxis.setTextColor(Color.RED); // 색 지정
+        xAxis.setDrawLabels(true); // 라벨(x축 좌표)를 그릴지 결정
+        xAxis.setDrawAxisLine(false); // x축 라인을 그림 (라벨이 없을때 잘 됨)
+        xAxis.setDrawGridLines(false); // 내부 선 그을지 결정
+        xAxis.setLabelCount(2); // 라벨의 개수를 결정 => 나누어 떨어지는 개수로 지정
         xAxis.setGranularity(1f); // minimum axis-step (interval) is 1
         xAxis.setValueFormatter(formatter);
 
@@ -81,7 +98,29 @@ public class MainActivity extends AppCompatActivity {
         dataSets.add(dataSet); dataSets.add(dataSet2);
         LineData lineData = new LineData(dataSets);
         lineChart.setData(lineData);
+        setAnimation();
         lineChart.invalidate(); // refresh
+
+        // 데이터가 변경되는 경우
+//        dataSet.notifyDataSetChanged(); // 데이터가 바뀌었다고 notify
+//        lineChart.notifyDataSetChanged(); // 차트에 데이터가 바뀌었다고 notify
+//        lineChart.invalidate(); // 다시 그리라고 명령
+
+        setCharStyling();
+    }
+
+    private void setCharStyling(){
+        lineChart.setBackgroundColor(Color.WHITE); // 배경색 지정
+        Description description = new Description();
+        description.setText("");
+        lineChart.setDescription(description);// 설명 정의(오른쪽 아래)
+        lineChart.setBorderWidth(100);
+//        lineChart.setMaxVisibleValueCount(8);
+        lineChart.invalidate();
+    }
+
+    private void setAnimation(){
+        lineChart.animateX(3000, Easing.EasingOption.Linear);
     }
 
     private void setInteraction(){
