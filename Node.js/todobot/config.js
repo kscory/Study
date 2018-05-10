31932b400ad1d2f5e5adc01d0996db93e01671d3
KyungSeok <leeseok8347@gmail.com>
2018-05-10 20:10:26 +0900

    [chatbot] papago config 추가

---
+++ Node.js/todobot/config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    papago: {
+        api_url : 'https://openapi.naver.com/v1/papago/n2mt',
+        client: {
+            id: '아이디',
+            secret: '시크릿'
+        }
+    }
+};
